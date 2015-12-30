5e1cccb2f46353038d3d7f345f93e9770163af25
Marc TECLES <mtecles@amg-dev.fr>
2015-12-30 14:09:05 +0100

    fix message

--- www/js/controllers/accountcontrollers.js
+++ www/js/controllers/accountcontrollers.js
@@ -13,15 +13,15 @@ angular.module('starter')
    };
   $scope.settings = Account.getsettings();
   $scope.check = function(url) {
-    var message = '', title = 'Web service check';
+    var message = 'Sorry but we can not contact the web service.', title = 'Web service check';
     var data;
     Account.ping(url).success( function(response) {
       console.log(response);
       data = response;
       message = (data.status == 'ok') ? 'Ok the web service url was successfully checked.' : 'Sorry but we can not contact the web service.'
-      $scope.showAlert(title, message);
 
     });
+    $scope.showAlert(title, message);
 
   };
 
