03d2f7337161a51da82c100c414a50879a07c9be
Marc <marc.tecles@gmail.com>
2015-12-30 13:14:19 +0100

    fix get issue

--- www/js/controllers/accountcontrollers.js
+++ www/js/controllers/accountcontrollers.js
@@ -14,10 +14,15 @@ angular.module('starter')
   $scope.settings = Account.getsettings();
   $scope.check = function(url) {
     var message = '', title = 'Web service check';
+    var data;
+    Account.ping(url).success( function(response) {
+      console.log(response);
+      data = response;
+      message = (data.status == 'ok') ? 'Ok the web service url was successfully checked.' : 'Sorry but we can not contact the web service.'
+      $scope.showAlert(title, message);
 
-    status = Account.ping(url);
-    message = (status == 'ok') ? 'Ok the web service url was successfully checked.' : 'Sorry but we can not contact the web service.'
-    $scope.showAlert(title, message);
+    });
+
   };
 
 });
--- www/js/services/accountservice.js
+++ www/js/services/accountservice.js
@@ -9,27 +9,12 @@ angular.module('starter')
   this.ping = function(url) {
     var rand = Math.floor((Math.random()*6)+1);
     console.log('pinging webservice : ' + url + '/controllers/ping/' + rand);
-    data = $http.get(url + "/controllers/ping/" + rand)
-      .success(function(data) {
-        if (data.status == 'ok') {
-          console.log('status ok');
-          return 'ok';
-        }
-
-      })
+    var data = $http.get(url + "/controllers/ping/" + rand)
       .error(function(data) {
           console.log('error');
-          return 'error'
       });
-
-    if (data.status == 'ok') {
-      console.log('status ok');
-      return 'ok';
-    }
-    else {
-      return 'error';
-    }
-
+    //console.log(data);
+    return data;
     };
 
   this.getsettings = function() {
