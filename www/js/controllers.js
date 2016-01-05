f2c345dde5dc84e2ba38f3c6ef2a2a8d42e3a48b
Marc <marc.tecles@gmail.com>
2016-01-05 13:31:56 +0100

    adds charts.js to dashboard

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,13 @@
-angular.module('domopi.controllers', [])
+angular.module('domopi.controllers', ['chart.js'])
 
-.controller('DashCtrl', function($scope) {});
+.controller('DashCtrl', function($scope) {
+  $scope.graph = {};
+  $scope.graph.data = [
+    //Awake
+    [16, 15, 20, 12, 16, 12, 8],
+    //Asleep
+    [8, 9, 4, 12, 8, 12, 14]
+  ];
+  $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  $scope.graph.series = ['Awake', 'Asleep'];
+});
